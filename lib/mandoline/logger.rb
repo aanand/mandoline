06840d53f4d3d67757eab18723626b730ba8dd97
Aanand Prasad <aanand.prasad@gmail.com>
2012-02-08 14:40:59 +0000

    Print file and scenario names in red

--- lib/mandoline/logger.rb
+++ lib/mandoline/logger.rb
@@ -5,15 +5,23 @@ module Mandoline
     end
 
     def file_deleted(path)
-      @io.puts("Deleted #{path}")
+      @io.puts("Deleted #{red(path)}")
     end
 
     def scenarios_deleted(path, names)
       @io.puts("Deleted scenarios from #{path}:")
 
       names.each do |name|
-        @io.puts("  #{name}")
+        @io.puts("  #{red(name)}")
       end
+    end
+
+    def red(text)
+      color(text, 31)
+    end
+
+    def color(text, code)
+      "\033[1;#{code}m#{text}\033[0m"
     end
   end
 end
