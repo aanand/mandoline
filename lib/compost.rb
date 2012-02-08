6cd254b3c56dffef6d7cf8148777c76d3f9d4103
Aanand Prasad <aanand.prasad@gmail.com>
2012-02-08 13:37:18 +0000

    Very basic command-line functionality

--- lib/compost.rb
+++ lib/compost.rb
@@ -1,5 +1,38 @@
 require "compost/version"
+require "compost/logger"
+require "compost/runner"
+require "optparse"
 
 module Compost
-  # Your code goes here...
+  def self.run(*args)
+    options = {}
+
+    parser = OptionParser.new do |opts|
+      opts.banner = "Usage: #{File.basename($0)} [OPTIONS] [PATHS]"
+
+      opts.on "-t TAGS", "--tags TAGS", "Comma-separated list of tags to delete, e.g. wip,pending,v1", Array do |v|
+        options[:tags] = v
+      end
+    end
+
+    begin
+      parser.parse!(args)
+      raise OptionParser::MissingArgument, "--tags" unless options[:tags]
+    rescue OptionParser::ParseError => e
+      puts e.message
+      puts
+      puts parser
+      exit -1
+    end
+
+    options[:logger] = Compost::Logger.new(STDERR)
+
+    paths = if args.length == 1 && File.directory?(args.first)
+              Dir.glob("#{args.first}/**/*.feature")
+            else
+              args
+            end
+
+    Runner.new(options).run(paths)
+  end
 end
---
+++ lib/compost/logger.rb
@@ -0,0 +1,19 @@
+module Compost
+  class Logger
+    def initialize(io)
+      @io = io
+    end
+
+    def file_deleted(path)
+      @io.puts("Deleted #{path}")
+    end
+
+    def scenarios_deleted(path, names)
+      @io.puts("Deleted scenarios from #{path}:")
+
+      names.each do |name|
+        @io.puts("  #{name}")
+      end
+    end
+  end
+end
